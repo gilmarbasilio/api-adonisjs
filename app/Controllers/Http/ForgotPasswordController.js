f10d733621bbc3a802b5f62f6ff944c6d0efc12c
GIlmar Basilio <gilmar.basilio@gmail.com>
2019-11-03 21:41:48 -0300

    Inicio do projeto

---
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const crypto = require('crypto')
+const User = use('App/Models/User')
+const Mail = use('Mail')
+
+class ForgotPasswordController {
+  async store ({ request, response, auth }) {
+    try {
+      const email = request.input('email')
+
+      const user = await User.findByOrFail('email', email)
+
+      user.token = crypto.randomBytes(10).toString('hex')
+      user.token_created_at = new Date()
+
+      await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        { email, token: user.token, link: `${request.input('redirect_url')}?token=${user.token}` },
+        message => {
+          message
+            .to(user.email)
+            .from('no-reply@nomady.com.br', 'Nomady')
+            .subject('Recuperação de senha')
+        }
+      )
+    } catch (error) {
+      return response.status(error.status).send({
+        error: {
+          message: 'Algo não deu certo, esse e-mail existe?'
+        }
+      })
+    }
+  }
+}
+
+module.exports = ForgotPasswordController
